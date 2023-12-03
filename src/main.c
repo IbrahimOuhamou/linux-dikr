9094f01038c18b5151b78ca5ce42b9577bb6bb4e
Ibrahim Ouhamou <ouhamouy10@gmail.com>
2023-12-03 13:19:21 +0100

    BismiAllah continuing with the proyotype

--- src/main.c
+++ src/main.c
@@ -10,14 +10,19 @@ const char* dikr_list_morning[] = {
     "أصبحت أثني عليك حمدا, و أشهد أنه لا إله إلا أنت"
 };
 
-const char** dikr_lists = dikr_list_morning;
+const char* dikr_list_evening[] = {
+    "رضيت بالله ربا و بالإسلام دينا و بمحمد نبيا و رسولا"
+};
+
+const char** dikr_list_selected_list = dikr_list_morning;
 
 void activate(GtkApplication* app, gpointer data);
 
 //selects dikr list ('dikr_lis_morning' for now)
-void dikr_list_select(GtkWidget* widget, gpointer data);
+void dikr_list_select_list(GtkWidget* widget, gpointer data);
 void dikr_list_menu(GtkWidget* widget, gpointer data);
 void dikr_list_select_next(GtkWidget* widget, gpointer data);
+void dikr_list_select_prev(GtkWidget* widget, gpointer data);
 
 static GtkWidget* window;
 
@@ -50,22 +55,45 @@ void dikr_list_menu(GtkWidget* widget, gpointer data)
     gtk_window_set_child(GTK_WINDOW(window), box);
 
     GtkWidget* button_morning_dikr = gtk_button_new_with_label("morning");
-    g_signal_connect(button_morning_dikr, "clicked", G_CALLBACK(dikr_list_select), NULL);
+    g_signal_connect(button_morning_dikr, "clicked", G_CALLBACK(dikr_list_select_list), dikr_list_morning);
+
+    GtkWidget* button_evening_dikr = gtk_button_new_with_label("evening");
+    g_signal_connect(button_evening_dikr, "clicked", G_CALLBACK(dikr_list_select_list), dikr_list_evening);
 
     gtk_box_append(GTK_BOX(box), button_morning_dikr);
+    gtk_box_append(GTK_BOX(box), button_evening_dikr);
 }
 
-void dikr_list_select(GtkWidget* widget, gpointer data)
+void dikr_list_select_list(GtkWidget* widget, gpointer data)
 {
+    if(NULL == dikr_list_selected_list)
+    {
+        dikr_list_menu(NULL, NULL);
+        return;
+    }
     //for now will select morning dikr only
     GtkWidget* box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_widget_set_valign(box, GTK_ALIGN_FILL);
+    gtk_widget_set_halign(box, GTK_ALIGN_FILL);
+    //gtk_box_set_homogeneous(GTK_BOX(box), TRUE);
+
     GtkWidget* label = gtk_label_new(dikr_list_morning[dikr_list_selected_id]);
     gtk_box_append(GTK_BOX(box), label);
     gtk_label_set_wrap(GTK_LABEL(label), TRUE);
 
+
+    GtkWidget* box_bottom = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    //gtk_widget_set_valign(box_bottom, GTK_ALIGN_END);
+    //gtk_widget_set_halign(box_bottom, GTK_ALIGN_FILL);
+    gtk_box_append(GTK_BOX(box), box_bottom);
+
     GtkWidget* button_dikr_list_select_next = gtk_button_new_with_label("<-");
     g_signal_connect(button_dikr_list_select_next, "clicked", G_CALLBACK(dikr_list_select_next), NULL);
-    gtk_box_append(GTK_BOX(box), button_dikr_list_select_next);
+    gtk_box_append(GTK_BOX(box_bottom), button_dikr_list_select_next);
+
+    GtkWidget* button_dikr_list_select_prev = gtk_button_new_with_label("->");
+    g_signal_connect(button_dikr_list_select_prev, "clicked", G_CALLBACK(dikr_list_select_prev), NULL);
+    gtk_box_append(GTK_BOX(box_bottom), button_dikr_list_select_prev);
 
     gtk_window_set_child(GTK_WINDOW(window), box);
 }
@@ -74,11 +102,20 @@ void dikr_list_select_next(GtkWidget* widget, gpointer data)
 {
     if(dikr_list_selected_id+1 >= (sizeof(dikr_list_morning) / sizeof(dikr_list_morning[0])))
     {
-        g_print("");
-        g_print("dikr_list_selected_id+1\nsizeof(dikr_list_morning) / sizeof(dikr_list_morning[0] = %ld\n", (sizeof(dikr_list_morning) / sizeof(dikr_list_morning[0])));
         return;
     }
 
     dikr_list_selected_id += 1;
-    dikr_list_select(NULL, NULL);
+    dikr_list_select_list(NULL, data);
+}
+
+void dikr_list_select_prev(GtkWidget* widget, gpointer data)
+{
+    if(0 == dikr_list_selected_id)
+    {
+        return;
+    }
+
+    dikr_list_selected_id-=1;
+    dikr_list_select_list(NULL, NULL);
 }
